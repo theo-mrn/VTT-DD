216c15ceb311890f17d373b1d864193c44558596
theo <theo@MacBook-Pro-de-theo.local>
2026-04-04 16:33:02 +0200

    update key + update readme

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -7,14 +7,14 @@ import { getDatabase, ref as dbRef, set, onValue, update, get as rtdbGet, push a
 import { getAnalytics, setAnalyticsCollectionEnabled } from 'firebase/analytics';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDrc70mfENCh6gCd5uJmeVbWJ98lcD6mQY",
-  authDomain: "test-b4364.firebaseapp.com",
-  databaseURL: "https://test-b4364-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "test-b4364",
-  storageBucket: "test-b4364.appspot.com",
-  messagingSenderId: "260245361856",
-  appId: "1:260245361856:web:99808b241e1a7c1e25c925",
-  measurementId: "G-TT348Z0BVP"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialisation de Firebase
